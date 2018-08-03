5e8ae0852d8ff06272500b821eeff8e3d7f020b2
Elizabeth Zarela Zanabria Chuquipiondo <zarelixzana@gmail.com>
2018-08-03 00:05:07 -0500

    Busca por tipos

--- src/js/appMaps.js
+++ src/js/appMaps.js
@@ -1,30 +1,33 @@
 var service;
 var infowindow;
-
+let positionMaps;
+let googleMaps;
 let mapGoogleOut = document.getElementById('googlemaps');
-let optionFood = document.getElementById('select-food');
+let optionFood;
 
 //evento para buscar los tipos de comida
-optionFood.addEventListener('change', event = () => {
-  alert('Seleccionastes');
+selectFood.addEventListener("change", function() {
+  photoPlace.innerHTML = '';
+  optionFood =selectFood.value;
+  mapsLocation(positionMaps);
 });
 
-//Creamos una funcion en donde ubicaion la latitud y longitud 
-//funcion que muestra el mapa
 function mapsLocation(posicion) {
+  positionMaps = posicion
   //Obtenermos las coordemadas
   let maplatitud =  -12.1167 /* posicion.coords.latitude */;
   let maplongitude = -77.0333/*  posicion.coords.longitude */;
 
-  var googleLatLon = new google.maps.LatLng(maplatitud, maplongitude);
-
-  var objConfig = {
+  let googleLatLon = new google.maps.LatLng(maplatitud, maplongitude); 
+  let objConfig = {
     zoom: 17,
     //center de aacuerdo una geolocalizacion
     center: googleLatLon
   }
 
-  let googleMaps = new google.maps.Map(mapGoogleOut, objConfig);
+   googleMaps = new google.maps.Map(mapGoogleOut, objConfig);
+
+ 
 
   let objConfigMarker = {
     //son objetos de google de acuerdo a las coordenadas 
@@ -34,11 +37,10 @@ function mapsLocation(posicion) {
   let request = {
     location: googleLatLon,
     radius: '500', /* range */
-    type: ['restaurant']
+    type: ['restaurant'],
+    keyword: optionFood, 
   }
   let googleMarker = new google.maps.Marker(objConfigMarker)
-
-
 
   //Ubica en el mapa los lugares de la busqueda por lugar 
   service = new google.maps.places.PlacesService(googleMaps);
@@ -48,29 +50,27 @@ function mapsLocation(posicion) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
       for (let i = 0; i < results.length; i++) {
         /* var place = results[i]; */
-        console.log(results[i])
+       
        createPhotoMarker(results[i]); 
         createMarker(results[i]);
       }
     }
   }
-
-  function createMarker(place) {
-    infowindow = new google.maps.InfoWindow();
-    /*  let placeLocation = place.geometry.location; */
-    let googleMarker = new google.maps.Marker({
-      icon: "img/food.png",
-      map: googleMaps,
-      position: place.geometry.location
-    });
-    google.maps.event.addListener(googleMarker, 'click', function () {
-      infowindow.setContent(place.name);
-      infowindow.open(googleMaps, this);
-    });
-  }
-
-
 }
+
+function createMarker(place) {
+  infowindow = new google.maps.InfoWindow();
+   let googleMarker = new google.maps.Marker({
+    icon: "img/food.png",
+    map: googleMaps,
+    position: place.geometry.location
+  });
+  google.maps.event.addListener(googleMarker, 'click', function () {
+    infowindow.setContent(place.name);
+    infowindow.open(googleMaps, this);
+  });
+}
+
 
 function error() {
   mapGoogleOut.innerHTML = "<p> No se puede ontener</p>";
@@ -81,14 +81,13 @@ navigator.geolocation.getCurrentPosition(mapsLocation, error);
 
 
 function createPhotoMarker(place) {
-  var photos = place.photos;
+ var photos = place.photos;
   if (!photos) {
     return;
-  }
-
-  let photoplace = photos[0].getUrl({ 'maxWidth': 180, 'maxHeight': 180 })
-  photoPlace.innerHTML += ` <div> 
-  <img src="${photoplace}">  
+  } 
+  let photoplace = photos[0].getUrl({ 'maxWidth': 160, 'maxHeight': 160 })
+  photoPlace.innerHTML += ` <div class="photoRestaurant"> 
+  <img  class="photoRestaurant" src="${photoplace}">  
   <p>${place.name}</p>
   </div>`;
 }
