ad9c9f9737b9b44eaf0406e880e1453cdb99c315
zarela zanabria <zarelixzana@gmail.com>
2018-10-10 11:04:06 -0500

    Splash

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
 window.onload=timeout;
 function timeout(){
 window.setTimeout("redirect()",2000)}
 
 function redirect(){
 window.location="foodmap.html"
+return
 }
-=======
-window.onload = timeout;
-function timeout() {
-    window.setTimeout("redirect()", 2000)
-}
-  function redirect() {
-    window.location = "foodmap.html"
-} 
- 
->>>>>>> 8074edee2abfe4957ee3a0a5bd475d311d83fe8d
 
 
 
